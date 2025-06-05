f04b6524ffb18c007866b13377c221a358c0f0a0
Dominick Goehrig <dgdomgoe3@gmail.com>
2025-06-05 19:35:00 -0400

    "looking for help"

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,7 +11,7 @@ import { coordinates, APIkey } from "../../utils/constants";
 import CurrentTemperatureUnitContext from "../../contexts/CurrentTemperatureUnitContext";
 import AddItemModal from "../AddItemModal/AddItemModal";
 import DeleteModal from "../DeleteModal/DeleteModal";
-import { getItems, deleteItems, addNewItems } from "../../utils/api";
+import { getItems, addItems, deleteItems } from "../../utils/api";
 function App() {
   const [weatherData, setWeatherData] = useState({
     type: "",
@@ -56,15 +56,16 @@ function App() {
     setSelectedCard(card);
   };
 
-  const handleDeleteItems = (_id) => {
-    deleteItems(selectedCard._id)
+  const HandledeleteItems = (id) => {
+    deleteItems(id)
       .then(() => {
         setClothingItems((prevItems) =>
-          prevItems.filter((item) => item._id !== selectedCard._id)
+          prevItems.filter((item) => item._id !== id)
         );
-        closeActiveModal();
       })
-      .catch(console.error);
+      .catch((err) => {
+        console.error(err);
+      });
   };
 
   useEffect(() => {
@@ -130,7 +131,7 @@ function App() {
         <DeleteModal
           isOpen={activeModal === "delete"}
           onClose={closeActiveModal}
-          onDelete={handleDeleteItems}
+          onDelete={HandledeleteItems}
         />
       </div>
     </CurrentTemperatureUnitContext.Provider>
--- src/utils/api.js
+++ src/utils/api.js
@@ -6,31 +6,20 @@ function getItems() {
   });
 }
 
-function addNewItems({ name, imageUrl, weather }) {
-  return fetch(`${baseUrl}/items/${_id}`, {
+function addItems({ name, imageUrl, weather }) {
+  return fetch(`${baseUrl}/items`, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/Json",
-    },
-    body: JSON.stringify({
-      name,
-      imageUrl,
-      weather,
-    }),
+    body: JSON.stringify({ name, imageUrl, weather }),
+    headers: { "Content-Type": application / json },
   }).then((res) => {
     return res.ok ? res.json() : Promise.reject(`Error ${res.status}`);
   });
 }
 
-function deleteItems(_id) {
-  return fetch(`${baseUrl}/items/${_id}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/Json",
-    },
-  }).then((res) => {
+function deleteItems() {
+  return fetch(`${baseUrl}/items/_id`, { method: "DELETE" }).then((res) => {
     return res.ok ? res.json() : Promise.reject(`Error ${res.status}`);
   });
 }
 
-export { getItems, deleteItems, addNewItems };
+export { getItems, addItems, deleteItems };
